73bcf97aee04d6ffd196146a31c216b86948aeeb
Iyii <iyiolaabifarin2@gmail.com>
2025-10-16 23:02:01 +0100

    Update owner-adddelowner.js

--- plugins/owner-adddelowner.js
+++ plugins/owner-adddelowner.js
@@ -1,25 +1,35 @@
 const handler = async (m, { conn, text, args, usedPrefix, command }) => {
-  const why = `${emoji} Por favo, menciona a un usuario pará agregar o quitar como owner.`;
-  const who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false;
-  if (!who) return conn.reply(m.chat, why, m, {mentions: [m.sender]});
+  const why = `${emoji} Please mention a user to add or remove as owner.`;
+  const who = m.mentionedJid[0] 
+    ? m.mentionedJid[0] 
+    : m.quoted 
+      ? m.quoted.sender 
+      : text 
+        ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' 
+        : false;
+
+  if (!who) return conn.reply(m.chat, why, m, { mentions: [m.sender] });
+
   switch (command) {
     case 'addowner':
       const nuevoNumero = who;
       global.owner.push([nuevoNumero]);
-      await conn.reply(m.chat, `${emoji} Listo Ya Está En La Lista De Owner El Usuario.`, m);
+      await conn.reply(m.chat, `${emoji} Done! The user has been added to the owner list.`, m);
       break;
+      
     case 'delowner':
       const numeroAEliminar = who;
       const index = global.owner.findIndex(owner => owner[0] === numeroAEliminar);
       if (index !== -1) {
         global.owner.splice(index, 1);
-        await conn.reply(m.chat, `${emoji2} Eliminado El Numero de la lista de owner correctamente.`, m);
+        await conn.reply(m.chat, `${emoji2} The number has been successfully removed from the owner list.`, m);
       } else {
-        await conn.reply(m.chat, `${emoji2} El Numero No Está En La Lista De Owners.`, m);
+        await conn.reply(m.chat, `${emoji2} The number is not in the owner list.`, m);
       }
       break;
   }
 };
-handler.command = ['addowner', 'delowner']
+
+handler.command = ['addowner', 'delowner'];
 handler.rowner = true;
 export default handler;
